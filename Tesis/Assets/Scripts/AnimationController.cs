cd40dd8ed8dca87e0ee952ed60f0b8f0b38ae69c
JorgeSebastianML <jsmora65@gmail.com>
2020-11-13 23:48:49 -0500

    Update in the selection of animation of the people, added counter of people asking

--- Tesis/Assets/Scripts/AnimationController.cs
+++ Tesis/Assets/Scripts/AnimationController.cs
@@ -5,37 +5,45 @@ using UnityEngine;
 public class AnimationController : MonoBehaviour
 {
     public int action = 0; 
-    public int probabilidad = 10;
+    public int probabilidad = 40;
     public string animationName = "Asking Question"; 
+    public float animationTime = 60; 
+    private float MaxTime;
     // Start is called before the first frame update
     void Start()
     {
-
+        MaxTime = animationTime + Random.Range(-30, 30); 
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (this.gameObject.GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).IsName(animationName))
+        MaxTime -= Time.deltaTime;
+        if (MaxTime <= 0)
         {
-            if(action == 0)
+            MaxTime = animationTime + Random.Range(-30, 30);
+            if (this.gameObject.GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).IsName(animationName))
             {
-                this.gameObject.transform.tag = "Pregunta";
+                if(action == 0)
+                {
+                    this.gameObject.transform.tag = "Pregunta";
+                }
+                else
+                {
+                    this.gameObject.transform.tag = "Alto";
+                }
+                
+                return; 
+            }
+            else if (Random.Range(0, 100) <= probabilidad)
+            {
+                this.gameObject.GetComponent<Animator>().Play(animationName);
             }
             else
             {
-                this.gameObject.transform.tag = "Alto";
+                this.gameObject.transform.tag = "Ninguna";
             }
-            
-            return; 
         }
-        else if (Random.Range(0, 1000) < probabilidad)
-        {
-            this.gameObject.GetComponent<Animator>().Play(animationName);
-        }
-        else
-        {
-            this.gameObject.transform.tag = "Ninguna";
-        }
+        
     }
 }
--- Tesis/Assets/Scripts/Control_characters.cs
+++ Tesis/Assets/Scripts/Control_characters.cs
@@ -6,6 +6,7 @@ public class Control_characters : MonoBehaviour
 {
     public int probabilityActivation = 20;
     private List<GameObject> characteres = new List<GameObject>();
+    private int nPersonas = 0; 
     // Start is called before the first frame update
     void Start()
     {
@@ -16,8 +17,18 @@ public class Control_characters : MonoBehaviour
             {
                 GameObject child = this.transform.GetChild(i).gameObject;
                 child.SetActive(true); 
+                if (child.GetComponent<AnimationController>() != null)
+                {
+                    AnimationController Animation = child.GetComponent<AnimationController>();
+                    if (Animation.action == 0)
+                    {
+                        nPersonas = nPersonas + 1; 
+                    }
+                }
+                
             }
         }
+        print("Numero de personas Preguntando son: " + nPersonas.ToString()); 
     }
 
     // Update is called once per frame
